5d286cb1d4525f3576ce99053f65775800eb19a9
NeumanGallardo <neuman10327033@gmail.com>
2024-05-19 20:24:38 -0300

    registerUser

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,15 @@ const {Router} = require("express");
 const userRouter = require("./userRouter");
 const suplementRouter = require("./suplementRouter");
 
+const loginRouter = require('./loginRouter');
+
+
 const routes = Router();
 
 //Rutas
 routes.use("/users", userRouter);
 routes.use("/suplements", suplementRouter);
+
+routes.use('/login', loginRouter);
 
 module.exports = routes;
