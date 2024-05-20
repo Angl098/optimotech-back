a0771e995902d61382fc15e0e49ff8a565161aa1
RoyerFrba <royeryl03@gmail.com>
2024-05-20 18:00:35 -0300

    alter = true

--- src/db.js
+++ src/db.js
@@ -13,8 +13,8 @@ const {
 //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 const sequelize = new Sequelize(DB_DEPLOY, {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
 
