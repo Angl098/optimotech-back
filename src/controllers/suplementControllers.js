65b64b2ab04559a4a08851cf695746e051d8c89b
Jose Tomas Cortina Espitia <joseto.corties@gmail.com>
2024-05-21 09:49:33 -0500

    primer commit

--- src/controllers/suplementControllers.js
+++ src/controllers/suplementControllers.js
@@ -1,10 +1,16 @@
-const { Suplement } = require('../db.js');
-const { Op } = require('sequelize');
+const { Suplement, Category } = require('../db.js');
+const { where, Op, fn, col } = require('sequelize');
 
 const {cleanInfoSuplements} = require('../utils/index');
 
 const getSuplements = async () => {
-    const suplements = await Suplement.findAll();
+    const suplements = await Suplement.findAll({include : [
+        {
+            model: Category,
+            attributes: ["id", "name"], // Incluye solo los atributos que necesitas
+            through: { attributes: [] }, // No incluye los atributos de la tabla intermedia
+        },
+    ]});
     const response = cleanInfoSuplements(suplements);
     return response
 }
@@ -26,13 +32,79 @@ const getSuplementById = async (id) => {
     return await Suplement.findByPk(id);
 }
 
-const createSuplement = async (name, category, description, price, image, amount) => {
-    return await Suplement.create({ name, category, description, price, image, amount });
+const createSuplement = async (suplement, category) => {
+    const [categoryCreated, created] = await Category.findOrCreate({
+        where: where(fn('LOWER', col('name')), Op.eq, category.toLowerCase()),
+        defaults: { name: category }
+    });
+
+    const suplementCreated = await Suplement.create(suplement);
+
+    await suplementCreated.addCategory(categoryCreated);
+
+    return suplementCreated;
+}
+const includeAll = (categoryId) => {
+    if (categoryId) {
+        return include = [
+            {
+                model: Category,
+                where: { id: categoryId },
+                attributes: ["id", "name"], // Incluye solo los atributos que necesitas
+                through: { attributes: [] }, // No incluye los atributos de la tabla intermedia
+            },
+        ]
+    }
+    else {
+        return include = [
+            {
+                model: Category,
+                attributes: ["id", "name"], // Incluye solo los atributos que necesitas
+                through: { attributes: [] }, // No incluye los atributos de la tabla intermedia
+            },
+        ]
+
+    }
+
 }
 
+const getFilteredSuplementsController = async (params) => {
+    const { category, orderBy, orderDirection } = params
+    let order = [];
+    if (orderBy && orderDirection) {
+        order = [[orderBy, orderDirection]]
+    }
+
+    let where = {};
+
+    // if (category) where = { ...where, category };
+
+    try {
+        // let include= includeAll(category)
+        let include = [];
+
+        if (category) {
+            include.push({
+                model: Category,
+                through: { attributes: [] }, // Esto excluye los atributos de la tabla intermedia
+                where: { id: category }
+            });
+        }
+
+
+        console.log("Query parameters:", { include, where, order });
+        const suplementsFiltered = await Suplement.findAll({ include, where, order });
+
+        return suplementsFiltered;
+
+    } catch (error) {
+        throw Error(error.message);
+    }
+};
 module.exports = { 
     getSuplements,
     getSuplementByName,
     getSuplementById, 
-    createSuplement 
+    createSuplement,
+    getFilteredSuplementsController
 }
--- src/models/SuplementsModel.js
+++ src/models/SuplementsModel.js
@@ -12,10 +12,6 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
             allowNull: false
         },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
         description: {
             type: DataTypes.TEXT,
             allowNull: false
