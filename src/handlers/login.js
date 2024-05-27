f05e3d71f0f25f4012a19bc7d1cf2dda25fb43ed
RoyerYampasi <royeryl03@gmail.com>
2024-05-27 19:25:44 -0300

    login id

--- src/handlers/login.js
+++ src/handlers/login.js
@@ -1,27 +1,27 @@
 const { login } = require('../controllers/login');
-const {  verifyPassword } = require('../utils/hashedPassword');
+const { verifyPassword } = require('../utils/hashedPassword');
 
 const loginHandler = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const usuario = await login(email);
-        if (!usuario) {
+        const user = await login(email);
+        if (!user) {
             return res.status(404).json({ mensaje: 'Usuario no encontrado' });
         }
 
-        // Validar contraseña
-        const validate = await verifyPassword(password, usuario.password);
+        // Validate password
+        const validate = await verifyPassword(password, user.password);
         if (!validate) {
             return res.status(401).json({ mensaje: 'Contraseña incorrecta' });
         }
 
-        // Si el usuario y la contraseña son válidos, enviar una respuesta exitosa
-        return res.status(200).json({ mensaje: 'Autenticación exitosa' });
+        // Successful authentication, return only the user ID
+        return res.status(200).json({ userId: user.id });
     } catch (error) {
         console.error('Error al autenticar usuario:', error);
         return res.status(500).json({ mensaje: 'Error interno del servidor' });
     }
-}
+};
 
-module.exports = { loginHandler }
+module.exports = { loginHandler };
