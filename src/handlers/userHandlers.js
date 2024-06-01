31887c3304fc85e76a0e7772ff04a264f9bc1c1c
Erik Neuman Gallardo Castillo <neuman10327033@gmail.com>
2024-06-01 15:31:29 -0300

    updateLoginRegister

--- src/handlers/userHandlers.js
+++ src/handlers/userHandlers.js
@@ -1,11 +1,20 @@
 const { createUser } = require('../controllers/userController');
 const {hashPassword}= require('..//utils/hashedPassword');
 
+const { loginController } = require('../controllers/loginController');
+
 const createUserHandler = async (req, res) => {
     const { name, sex, email, cellphone, address } = req.body;
     let {password} =  req.body;
     password = await hashPassword(password);
     try {
+        //verificar que el email no se encuentre registrado
+        const user = await loginController(email);
+        console.log('email en la DB', user);
+        if (user) {
+            return res.status(200).json({ message: 'El Email ya esta registrado "Inicia sesion"´' });
+          }
+
         await createUser(name, sex, email, password, cellphone, address);
         res.status(200).json({ message: 'Registro de usuario Exitoso' });
     } catch (error) {
