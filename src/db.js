6822b9dc9e9b349a0987b05eb4e5001f380941cb
RoyerYampasi <royeryl03@gmail.com>
2024-06-03 12:33:23 -0300

    merge

--- src/db.js
+++ src/db.js
@@ -40,7 +40,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Suplement, User, Category, Orden, OrdenSuplement, Cart, CartSuplements } = sequelize.models;
+const { Suplement, User, Category, Orden,Provider, Tag, OrdenSuplement, Cart, CartSuplements } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
